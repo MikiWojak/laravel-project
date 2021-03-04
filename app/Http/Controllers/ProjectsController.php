e47479e1e60441aa0d94b716c0a58546b4a4102e
MikiWojak <mikizarowa8@gmail.com>
2021-03-04 17:13:21 +0100

    Return projects
    - Return projects from databse in JSON format

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -3,10 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Project;
 
 class ProjectsController extends Controller
 {
-    public function index() {      
+    public function index() {
+      $projects = Project::all();
+
+      return $projects;
+
       return view('projects.index');
     }
 }
