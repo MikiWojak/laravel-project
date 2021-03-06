5f8dd4038b7d8c81bb78ab5eee6e8e3d18f3e413
MikiWojak <mikizarowa8@gmail.com>
2021-03-06 19:12:04 +0100

    Simply method and csrf
    - Simplify method and csrf

--- resources/views/projects/edit.blade.php
+++ resources/views/projects/edit.blade.php
@@ -4,11 +4,8 @@
   <h1>Edit Project</h1>
 
   <form action="/projects/{{ $project->id }}" method="post">
-    <!-- Transform POST to PATCH -->
-    {{ method_field('patch') }}
-
-    <!-- Must-have when parse data in Laravel -->
-    {{ csrf_field() }}
+    @method('PATCH')
+    @csrf
 
     <div>
       <input type="text" name="title" placeholder="Project title" value="{{ $project->title }}">
@@ -23,8 +20,8 @@
 
   <form action="/projects/{{ $project->id }}" method="POST">
     <div>
-      {{ method_field('DELETE') }}
-      {{ csrf_field() }}
+      @method('DELETE')
+      @csrf
 
       <button type="submit">Delete Project</button>
     </div>
